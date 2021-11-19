e5590766ca02503c79f6d78ea4ba5cb41ffee784
HosseinGhiasi <hosseinghiasi5727@gmail.com>
2021-11-19 23:23:23 +0330

    update

--- src/components/banner/MainBanner.js
+++ src/components/banner/MainBanner.js
@@ -5,6 +5,7 @@ import { Trans, useTranslation } from 'react-i18next';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import Img1 from '../../assets/image/hamber1.svg'
 import Img2 from '../../assets/image/hamber2.svg'
+import Img3 from '../../assets/image/hamber3.svg'
 
 function MainBanner({ setIsPersian, isPersian }) {
 
@@ -30,6 +31,10 @@ function MainBanner({ setIsPersian, isPersian }) {
                 <h1 className="text-4xl sm:text-5xl">{t("description")}</h1>
                 <p className="my-12  text-lg sm:text-xl">{t("description2")}</p>
                 <Button style={{ marginTop: '10px', width: '240px', fontSize: isPersian && "22px" }} variant="contained" color="error">{t("textButton")}<NavigateNextIcon className="ml-2 text-2xl" /></Button>
+
+                <div className="flex justify-end" style={{ marginRight: '-160px' }}>
+                    <img src={Img3} className="w-72 transform  -rotate-12" />
+                </div>
 
                 <div className=" justify-evenly mt-2  flex-wrap hidden sm:flex">
                     <img src={Img1} className="w-48" />
